dbcd2dfc8fa577df01efed23ea8fcc531eeb865d
ahmedkhafaga621 <ahmedkhafaga621@icloud.com>
2024-05-04 21:43:29 +0300

    upload

---
+++ twet counter .js
@@ -0,0 +1,11 @@
+var tweet_chrac = prompt("please enter a tweet ") ; 
+var check_tweet = tweet_chrac.slice(0,140); 
+alert(check_tweet);
+
+// or 
+// alert(prompt("please enter your tweet ").slice(0,140));
+
+// dog age = human 
+// var dogAge = prompt("please enter humam age ") ; 
+// var humanAge = (dogAge - 2 ) *4 + 21 ; 
+// alert(humanAge)
